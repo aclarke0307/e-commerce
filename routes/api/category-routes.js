c510de7f4424ba4c7c85273cc6171c516620abbe
aclar <a.clarke0307@gmail.com>
2021-09-19 19:04:23 -0400

    api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,80 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try{
+    const d = await Category.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(d);
+  } catch (err){
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+    try {
+      const d = await Category.findByPk(req.params.id, {
+        include: [{model: Product}]
+      })
+      if(!d){
+        res.status(404).json({message: 'Can not find category with this ID'});
+      } else {
+        res.status(200).json(d);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const d = await Category.create(req.body);
+    res.status(200).json(d);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const d = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    if(!d[0]){
+      res.status(404).json({message: 'can not find a category with this ID'});
+    }else {
+      res.status(200).json(d);
+    }
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const d = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if(!d){
+      res.status(404).json({message: 'can not find a category with this ID'});
+    } else {
+      res.status(200).json(d);
+    }
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,55 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
+  try {
+    const d = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
+        }
+      ]
+    })
+    res.status(200).json(d);
+  }catch(err){
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const d = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
+        }
+      ]
+    })
+    if(!d){
+      res.status(404).json({message: 'can not finf product ID'});
+    }else {
+      res.status(200).json(d);
+    }
+  }catch (err){
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
@@ -89,8 +130,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',  async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const d = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if(!d){
+      res.status(404).json({message: 'can not find procuct by this ID'});
+    }else {
+      res.status(200).json(d);
+    }
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
